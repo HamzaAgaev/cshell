9f2304fdef48bf6c1d9e0d4264bfdc59fb447fe2
hamzaagaev <hamzaagaev@gmail.com>
2025-02-10 01:13:54 +0300

    Try to fix error with disabling cache in pipeline

--- cache-library/source/lab2.c
+++ cache-library/source/lab2.c
@@ -40,15 +40,21 @@ void destroyLRUCache() {
 }
 
 int lab2Open(const char *path) {
-    int fd = open(path, O_SYNC | O_RDWR | O_CREAT, RW_ALL);
-    if (fd == -1) {
-        return -1;
-    }
-    int fcntl_ = fcntl(fd, F_NOCACHE, 1);
-    if (fcntl_ == -1) {
-        return -1;
-    }
-    return fd;
+    #ifdef __APPLE__
+        int fd = open(path, O_SYNC | O_RDWR | O_CREAT, RW_ALL);
+        if (fd == -1) {
+            return -1;
+        }
+        int fcntl_ = fcntl(fd, F_NOCACHE, 1);
+        if (fcntl_ == -1) {
+            return -1;
+        }
+        return fd;
+    #elif defined(__linux__)
+        return open(path, O_SYNC | O_RDWR | O_CREAT | O_DIRECT, RW_ALL);
+    #else
+        return open(path, O_SYNC | O_RDWR | O_CREAT, RW_ALL);
+    #endif
 }
 
 int lab2Close(int fd) {
