9624afe23f5d21b8c5fa1acae75e30a7ffa93a82
hamzaagaev <hamzaagaev@gmail.com>
2025-02-12 19:06:51 +0300

    Fix benchmarkChecker.py

--- benchmarkChecker.py
+++ benchmarkChecker.py
@@ -10,27 +10,32 @@ def find_file_by_pattern(pattern):
     return found_files[0]
 
 
-def check_is_sorted_file(file):
-    numbers_count = file.readline()
-    numbers = list(map(int, file.readline().split()))
-    return numbers == sorted(numbers)
+def check_is_sorted_file(input_file_, output_file_):
+    if input_file_.readline() != output_file_.readline():
+        return False
+    input_numbers = list(map(int, input_file_.readline().split()))
+    output_numbers = list(map(int, output_file_.readline().split()))
+    return output_numbers == sorted(input_numbers)
 
 
-def check_is_dedup_file(file):
-    numbers_count = file.readline()
-    numbers = list(map(int, file.readline().split()))
-    return Counter(numbers) == Counter(set(numbers))
+def check_is_dedup_file(input_file_, output_file_):
+    if input_file_.readline() != output_file_.readline():
+        return False
+    input_numbers = list(map(int, input_file_.readline().split()))
+    output_numbers = list(map(int, output_file_.readline().split()))
+    return Counter(output_numbers) == Counter(set(output_numbers))
 
 
 if __name__ == "__main__":
     benchmark = argv[1]
-    output_pattern = argv[2]
-    outputFile = open(find_file_by_pattern(output_pattern), "r")
+    directory_pattern = argv[2]
+    input_file = open(find_file_by_pattern(directory_pattern + "/input.txt"), "r")
+    output_file = open(find_file_by_pattern(directory_pattern + "/*output.txt"), "r")
     is_success = False
     if benchmark == "benchmark-1":
-        is_success = check_is_sorted_file(outputFile)
+        is_success = check_is_sorted_file(input_file, output_file)
     elif benchmark == "benchmark-2":
-        is_success = check_is_dedup_file(outputFile)
+        is_success = check_is_dedup_file(input_file, output_file)
     if not is_success:
         raise Exception(f"{benchmark} отрабатывает неправильно.")
     print("Done!")
